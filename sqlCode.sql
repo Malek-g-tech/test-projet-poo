1cb62824ad4f906b39c99f4a0d8508ce93ab7eb2
Talel Zighni <talelzigni24@gmai.com>
2026-01-17 19:14:55 +0000

    nzid mat3 malek elli na7etoo bel 5alet

---
+++ sqlCode.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE IF NOT EXISTS pharmacie;
+USE pharmacie;
+
+
+CREATE TABLE IF NOT EXISTS Medicament (
+    id_m INT AUTO_INCREMENT PRIMARY KEY,
+    libelle varchar(50),
+    quantite INT,
+    prix FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS Fournisseur (
+    id_f INT AUTO_INCREMENT PRIMARY KEY,
+    nom_fournissuer varchar(50)
+);
+
+CREATE TABLE IF NOT EXISTS Client (
+    id_c INT AUTO_INCREMENT PRIMARY KEY,
+    nom_client varchar(50)
+);
+
+CREATE TABLE IF NOT EXISTS Commandes(
+    id_m INTEGER ,
+    id_f INTEGER ,
+    dateC DATETIME NOT NULL,
+    quantite INTEGER NOT NULL,
+    CONSTRAINT c_pk PRIMARY KEY(id_m,id_f,dateC),
+    CONSTRAINT for_med FOREIGN KEY(id_m) REFERENCES Medicament(id_m) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT for_fou FOREIGN KEY(id_f) REFERENCES Fournisseur(id_f) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS Vente(
+    id_m INTEGER ,
+    id_c INTEGER ,
+    dateV DATETIME NOT NULL,
+    quantite INTEGER NOT NULL,
+    CONSTRAINT v_pk PRIMARY KEY(id_m,id_c,dateV),
+    CONSTRAINT for_med FOREIGN KEY(id_m) REFERENCES Medicament(id_m) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT for_fou FOREIGN KEY(id_c) REFERENCES Client(id_c) ON DELETE CASCADE ON UPDATE CASCADE
+
+);
